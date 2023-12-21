326474332c4fdd2926dbeb1202803141d321066f
DiviyamPathak <patdiviyam@gmail.com>
2023-12-22 01:05:59 +0530

    all done

--- bookscrape/spiders/quoter.py
+++ bookscrape/spiders/quoter.py
@@ -9,8 +9,12 @@ class QuoterSpider(scrapy.Spider):
                  ]
 
     def parse(self, response):
-        quotes = response.css(".quote .text::text").getall()
-
-        yield {
-            "text":quotes
-        }
+        for quote in response.css("div.quote"):
+            yield {
+                "text": quote.css("span.text::text").get(),
+                "auth": quote.css("span small::text").get(),
+                "tag": quote.css("div.tags a.tag::text").getall(),
+            }
+        nextpage = response.css("li.next a::attr(href)").get()
+        if nextpage is not None:
+            yield response.follow(nextpage,callback=self.parse)
